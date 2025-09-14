d3d26eab90bd501821da5594f613d470d4238d6f
momkingtai <momkingtai@gmail.com>
2025-09-15 01:06:22 +0530

    add client and modified server

--- Nishant/client.cpp
+++ Nishant/client.cpp
@@ -1,4 +1,12 @@
 #include <stdint.h>
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+using namespace std;
 
 typedef struct {
     uint16_t magic;
@@ -9,3 +17,22 @@ typedef struct {
     int64_t logical_clock;
     int64_t timestamp;
 } UAP_header;
+
+int main(int argc, char** argv)
+{
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(atoi(argv[2]));
+
+    inet_pton(AF_INET, argv[1], &serverAddress.sin_addr);
+
+    int clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if(clientSocket < 0) { perror("socket"); return 1; }
+
+    int sent = sendto(clientSocket, "Hello", 5, 0,
+                    (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    if(sent < 0) { perror("sendto"); return 1; }
+    else { cout << "Sent " << sent << " bytes" << endl; }
+
+    return 0;
+}
--- Nishant/server.cpp
+++ Nishant/server.cpp
@@ -3,10 +3,11 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 using namespace std;
 
-void serverCreateAddr(sockaddr_in& serverAddress, int portNo)
+void createServerAddr(sockaddr_in& serverAddress, int portNo)
 {
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(portNo);
@@ -19,6 +20,28 @@ int main (int argc, char** argv)
 
     int serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
 
-    sockaddr_in serverAddress;
-    serverCreateAddr(serverAddress, portNo);
+    struct sockaddr_in serverAddress;
+    createServerAddr(serverAddress, portNo);
+
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    char buffer[50];
+    sockaddr_in clientAddress;
+    socklen_t size = sizeof(clientAddress);
+
+    cout << "Server waiting for a packet..." << endl;
+    int n = recvfrom(serverSocket, buffer, sizeof(buffer)-1, 0,
+                    (struct sockaddr*)&clientAddress, &size);
+    if(n < 0) { perror("recvfrom"); return 1; }
+    cout << "recvfrom returned: " << n << " bytes" << endl;
+
+    buffer[n] = '\0';
+    cout << "Received: " << buffer << endl;
+
+    char ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &clientAddress.sin_addr, ip, sizeof(ip));
+    cout << "From client: " << ip << ":" << ntohs(clientAddress.sin_port) << endl;
+
+
+    return 0;
 }
