a769d290d704b8797e69312cc5e641776cee115a
momkingtai <momkingtai@gmail.com>
2025-09-14 21:40:24 +0530

    added header structure for UAP

--- Nishant/server.cpp
+++ Nishant/server.cpp
@@ -1,5 +1,29 @@
 #include <iostream.h>
+#include <stdint.h>
 
-int main(){
-    
+typedef struct {
+    uint16_t magic;
+    uint8_t version;
+    uint8_t command;
+    int32_t sequence_number;
+    int32_t session_id;
+    int64_t logical_clock;
+    int64_t timestamp;
+} UAP_header;
+
+void serverAddr (sockaddr_in& serverAddress, int portNo)
+{
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(portNo);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+}
+
+int main (int argc, **char argv)
+{
+    int portNo = argv[1];
+
+    int serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    sockaddr_in serverAddress;
+    serverAddr(serverAddress, portNo);
 }
